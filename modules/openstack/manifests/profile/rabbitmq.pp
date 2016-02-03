f930ba782b5165203d2a92e41ef20ebfa8a8c060
louismariel <louismariel@hotmail.com>
2016-02-03 20:48:00 +0100

    adding puppet modules

---
+++ modules/openstack/manifests/profile/rabbitmq.pp
@@ -0,0 +1,31 @@
+# The profile to install rabbitmq and set the firewall
+class openstack::profile::rabbitmq {
+  $management_address = $::openstack::config::controller_address_management
+
+  if $::osfamily == 'RedHat' {
+    package { 'erlang':
+      ensure  => installed,
+      before  => Package['rabbitmq-server'],
+      require => Yumrepo['erlang-solutions'],
+    }
+  }
+
+  rabbitmq_user { $::openstack::config::rabbitmq_user:
+    admin    => true,
+    password => $::openstack::config::rabbitmq_password,
+    provider => 'rabbitmqctl',
+    require  => Class['::rabbitmq'],
+  }
+  rabbitmq_user_permissions { "${openstack::config::rabbitmq_user}@/":
+    configure_permission => '.*',
+    write_permission     => '.*',
+    read_permission      => '.*',
+    provider             => 'rabbitmqctl',
+  }->Anchor<| title == 'nova-start' |>
+
+  class { '::rabbitmq':
+    service_ensure    => 'running',
+    port              => 5672,
+    delete_guest_user => true,
+  }
+}
---
+++ modules/tempest/README.markdown
@@ -0,0 +1,31 @@
+Tempest
+=======
+
+5.1.0 - 2014.2 - Juno
+
+Module for installing and configuring tempest.
+
+Tempest is the test suite that can be used to run integration
+tests on an installed openstack environment.
+
+This module assumes the provisioning of the initial OpenStack
+resources has been done beforehand.
+
+Release Notes
+-------------
+
+** 5.1.0 **
+
+* spec: pin rspec-puppet to 1.0.1
+* Update .gitreview file for project rename
+* Allow to not manage Tempest code in Puppet
+* Allow to activate Ceilometer tests
+
+** 5.0.0 **
+
+* Stable Juno release
+* Pinned vcsrepo dependency to 2.x
+* Bumped stdlib dependency to 4.x
+* Added ability to hide secrets from puppet logs
+* Removed orphaned os_concat function
+* Removed dependencies on mysql and postgresql devel libraries
